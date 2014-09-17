4c21cf687a29518726da39dd0cfcb721bfe306d1
yurets19 <yuraakayes@gmail.com>
2014-09-17 15:42:21 +0400

    initial commit

---
+++ cs_ncc_def.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace ycore.ncc
+{
+    public class TModifyManager
+    {
+        public TModifyManager()
+        {
+        }
+        public void ProceedXMLJob(string XMLFileName)
+        {
+            MessageBox.Show(XMLFileName);
+        }
+    }
+}
---
+++ cs_tk_log.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace tk4000.log
+{
+    abstract public class TCustomLog
+    {
+        protected abstract void AddInternal( string s, bool flag_newline);
+        public virtual void Add(string S)
+        {
+            AddInternal(S,true);
+            fStartLine="";
+        }
+        public virtual void AddError( string S)
+        {
+            Add(S);
+        }
+        public virtual void AddOK( string S="OK")
+        {
+            Add(S);
+        }
+        public bool RaiseOnError { get;set;}
+      
+        public string fStartLine;
+
+    
+
+    }
+    public class TNullLog : TCustomLog
+    {
+        protected override void AddInternal(string s, bool flag_newline)
+        {
+            
+        }
+    }
+    public class TConsoleLog : TCustomLog
+    {
+        protected override void AddInternal(string s, bool flag_newline)
+        {
+            if (flag_newline) { Console.WriteLine(s); } else { Console.Write(s); };
+        }
+        public override void AddError(string S)
+        {
+            Add("***"+S);
+            if (RaiseOnError) throw new Exception(S);
+        }
+    }
+
+
+}
